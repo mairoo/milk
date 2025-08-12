f19d61692538ccb71b0d999420becc0b4042ef7a
mairoo <jonghwa@pincoin.co.kr>
2025-08-13 00:14:17 +0900

    index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit'
-import counterReducer from './slices/counterSlice'
+import counterReducer from './test/counterSlice'
 
 export const store = configureStore({
     reducer: {
