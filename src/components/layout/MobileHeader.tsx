1b0bc6faeb7561cdab8b72a44d723121a11eb4a6
mairoo <jonghwa@pincoin.co.kr>
2025-08-15 20:44:20 +0900

    bigger button

--- src/components/layout/MobileHeader.tsx
+++ src/components/layout/MobileHeader.tsx
@@ -12,12 +12,16 @@ interface MobileHeaderProps {
 }
 
 export default function MobileHeader({className}: MobileHeaderProps) {
+    // sticky 요소가 동시에 flex container가 되면, flex 레이아웃 계산과 sticky positioning 계산이 서로 간섭
+    // sticky position만 담당하는 요소와 가로 배치 div 요소 분리
+    // 버튼 테두리는 variant="outline"
+    // 버튼 아이콘 이미지 크기는 size-*로 동작
     return (
         <header className={cn("sticky top-0 bg-white", className)}>
             <div className="flex items-center p-2">
                 <div>
-                    <Button variant="outline" size="icon">
-                        <Menu className="h-5 w-5"/>
+                    <Button variant="outline" size="icon" className="h-10 w-10">
+                        <Menu className="size-5"/>
                         <span className="sr-only">메뉴</span>
                     </Button>
                 </div>
@@ -33,8 +37,8 @@ export default function MobileHeader({className}: MobileHeaderProps) {
                     </Link>
                 </div>
                 <div>
-                    <Button variant="outline" size="icon">
-                        <ShoppingCart className="h-5 w-5"/>
+                    <Button variant="outline" size="icon" className="h-10 w-10">
+                        <ShoppingCart className="size-5"/>
                         <span className="sr-only">장바구니</span>
                     </Button>
                 </div>
