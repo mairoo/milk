4f480e4d37c4df4702f0585f56eccaf3a73d7b3a
mairoo <jonghwa@pincoin.co.kr>
2025-08-15 14:27:03 +0900

    category items

---
+++ src/global/types/constants.ts
@@ -0,0 +1,45 @@
+export const categoryItems = [
+    {
+        title: "구글/넥슨/퍼니카드",
+        items: [
+            {name: "구글기프트카드", href: "/gift-cards/google-play"},
+            {name: "넥슨카드", href: "/gift-cards/nexon"},
+            {name: "퍼니카드", href: "/gift-cards/funny-card"},
+        ]
+    },
+    {
+        title: "스마트/도서문화/컬쳐랜드",
+        items: [
+            {name: "컬쳐랜드상품권", href: "/gift-cards/cultureland"},
+            {name: "도서문화상품권", href: "/gift-cards/book-culture"},
+            {name: "스마트문화상품권", href: "/gift-cards/smart"},
+        ]
+    },
+    {
+        title: "에그머니",
+        items: [
+            {name: "에그머니", href: "/gift-cards/egg-money"},
+        ]
+    },
+    {
+        title: "틴캐시",
+        items: [
+            {name: "틴캐시", href: "/gift-cards/teen-cash"},
+        ]
+    },
+    {
+        title: "선불쿠폰",
+        items: [
+            {name: "스타벅스", href: "/coupons/mobile"},
+            {name: "숲 별풍선", href: "/coupons/convenience"},
+            {name: "플레이스테이션", href: "/coupons/online"},
+            {name: "와우캐시", href: "/coupons/online"},
+            {name: "N코인", href: "/coupons/online"},
+            {name: "요기요", href: "/coupons/online"},
+            {name: "한게임상품권", href: "/coupons/online"},
+            {name: "매니아선불쿠폰", href: "/coupons/online"},
+            {name: "아이템베이선불쿠폰", href: "/coupons/online"},
+        ]
+    }
+];
+
