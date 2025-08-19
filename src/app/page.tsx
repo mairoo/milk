0b45b07cd037f180081fdd2f92dd05d66e4b7a4c
mairoo <jonghwa@pincoin.co.kr>
2025-08-19 16:58:43 +0900

    home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
     }
     return (
         <div className="flex flex-col gap-y-6">
-            <Section title="오늘의 상품권" className="">
+            <Section title="오늘의 상품권">
                 <div className="grid grid-cols-2 md:grid-cols-6 gap-2 md:gap-6">
                     {todayCategoryData.map((category) => (
                         <CategoryCard
@@ -41,6 +41,28 @@ export default function Home() {
                     ))}
                 </div>
             </Section>
+            <div className="flex flex-col md:flex-row gap-6">
+                <Section title="상품권 금융사기 예방 수칙">
+                    <ul className="space-y-2 mb-2">
+                        <li>다른 사람으로부터 상품권 구매로 일부 또는 전체 금액을 입금 받기로 했습니까?</li>
+                        <li>상품권 일부 또는 전체를 <strong className="text-red-600">대리구매</strong> 하여 카카오톡 등 메신저로 다른 사람에게 주기로 했습니까?</li>
+                        <li>네이트온/카카오톡 등 메신저에서 지인이 급한 돈이 필요하다고 상품권을 요구했습니까?</li>
+                        <li>중고나라 또는 번개장터에서 물품대금을 현금 대신 상품권으로 요구 받았습니까?</li>
+                    </ul>
+                    <p className="text-red-600">위 질문 중 하나라도 해당하면 사기꾼과 메신저 또는 전화 연락을 끊고 바로 경찰서에 연락하시기 바랍니다.</p>
+                </Section>
+                <Section>
+                    광고
+                </Section>
+            </div>
+            <div className="flex flex-col md:flex-row gap-6">
+                <Section title="공지사항">
+                    -
+                </Section>
+                <Section title="핀코인 이용후기">
+                    -
+                </Section>
+            </div>
         </div>
     )
 }
